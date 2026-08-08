ba80e3f86fff640446f87e6b3fcbfbb4fb29cd9b
Ayush Shrivas <ayushshrivas168@gmail.com>
2026-08-08 08:52:29 +0530

    Delete src directory
    fix: bug fixing

--- src/routes/planner.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const plannerController = require("../controllers/planner.controller");
-
-router.get("/:missionId", plannerController.getMissionPlan);
-
-module.exports = router;
--- src/routes/procedure.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-const procedureController = require("../controllers/procedure.controller");
-
-router.get("/:type", procedureController.getProcedure);
-
-module.exports = router;
